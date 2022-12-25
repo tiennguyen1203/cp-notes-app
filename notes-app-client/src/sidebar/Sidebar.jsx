54f5d763906d98b1f8fa9f27d36f0ad672b749da
tiennguyen1203 <01228129873a@gmail.com>
2022-12-25 11:25:00 +0700

    [Client] Add No Notes state

--- notes-app-client/src/sidebar/Sidebar.jsx
+++ notes-app-client/src/sidebar/Sidebar.jsx
@@ -14,27 +14,31 @@ const Sidebar = ({
         <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
-        {sortedNotes.map(({ id, title, body, updatedAt }, i) => (
-          <div
-            className={`app-sidebar-note ${id === activeNote && "active"}`}
-            onClick={() => setActiveNote(id)}
-            key={id}
-          >
-            <div className="sidebar-note-title">
-              <strong>{title}</strong>
-              <button onClick={e => onDeleteNote(id)}>Delete</button>
-            </div>
+        {sortedNotes?.length ? (
+          sortedNotes.map(({ id, title, body, updatedAt }, i) => (
+            <div
+              className={`app-sidebar-note ${id === activeNote && "active"}`}
+              onClick={() => setActiveNote(id)}
+              key={id}
+            >
+              <div className="sidebar-note-title">
+                <strong>{title}</strong>
+                <button onClick={e => onDeleteNote(id)}>Delete</button>
+              </div>
 
-            <p>{body && body.substr(0, 100) + "..."}</p>
-            <small className="note-meta">
-              Last Modified{" "}
-              {new Date(updatedAt).toLocaleDateString("en-GB", {
-                hour: "2-digit",
-                minute: "2-digit"
-              })}
-            </small>
-          </div>
-        ))}
+              <p>{body && body.substr(0, 100) + "..."}</p>
+              <small className="note-meta">
+                Last Modified{" "}
+                {new Date(updatedAt).toLocaleDateString("en-GB", {
+                  hour: "2-digit",
+                  minute: "2-digit"
+                })}
+              </small>
+            </div>
+          ))
+        ) : (
+          <div className="no-notes">No Notes</div>
+        )}
       </div>
     </div>
   );
